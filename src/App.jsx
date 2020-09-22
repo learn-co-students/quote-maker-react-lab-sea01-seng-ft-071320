3076950401f5cfc16d1a4236c8f30033149f87c8
Alec DuBois <after.alec@gmail.com>
2020-09-22 15:35:56 -0700

    Done.

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import QuoteForm from "./components/QuoteForm";
+import Quotes from "./containers/Quotes";
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className="container-fluid">
+        <div
+          className="row title justify-content-center"
+          style={{ paddingTop: "12px" }}
+        >
+          <h1>Quote Maker</h1>
+        </div>
+        <hr />
+        <QuoteForm />
+        <Quotes />
+      </div>
+    );
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux'
-import rootReducer from './reducers/index'
-
-let store = createStore(rootReducer)
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { Provider } from "react-redux";
+import { store } from "./store";
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <App store={store} />
   </Provider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
